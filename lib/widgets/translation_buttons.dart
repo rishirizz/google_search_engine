fccfb68913be2fd683c65ebd38fa2412ce32a82e
arijeet <arijeetc6@gmail.com>
2024-02-28 13:36:06 +0530

    translation text spacing corrected if the width is reduced.

--- lib/widgets/translation_buttons.dart
+++ lib/widgets/translation_buttons.dart
@@ -6,29 +6,32 @@ class TranslationButtons extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Wrap(
-      alignment: WrapAlignment.center,
-      children: [
-        Text('Google Offered in:'),
-        SizedBox(width: 8),
-        LanguageText(title: 'हिन्दी'),
-        SizedBox(width: 8),
-        LanguageText(title: 'বাংলা'),
-        SizedBox(width: 8),
-        LanguageText(title: 'తెలుగు'),
-        SizedBox(width: 8),
-        LanguageText(title: 'मराठी'),
-        SizedBox(width: 8),
-        LanguageText(title: 'தமிழ்'),
-        SizedBox(width: 8),
-        LanguageText(title: 'ગુજરાતી'),
-        SizedBox(width: 8),
-        LanguageText(title: 'ಕನ್ನಡ'),
-        SizedBox(width: 8),
-        LanguageText(title: 'മലയാളം'),
-        SizedBox(width: 8),
-        LanguageText(title: 'ਪੰਜਾਬੀ'),
-      ],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      child: Wrap(
+        alignment: WrapAlignment.center,
+        children: [
+          Text('Google Offered in:'),
+          SizedBox(width: 8),
+          LanguageText(title: 'हिन्दी'),
+          SizedBox(width: 8),
+          LanguageText(title: 'বাংলা'),
+          SizedBox(width: 8),
+          LanguageText(title: 'తెలుగు'),
+          SizedBox(width: 8),
+          LanguageText(title: 'मराठी'),
+          SizedBox(width: 8),
+          LanguageText(title: 'தமிழ்'),
+          SizedBox(width: 8),
+          LanguageText(title: 'ગુજરાતી'),
+          SizedBox(width: 8),
+          LanguageText(title: 'ಕನ್ನಡ'),
+          SizedBox(width: 8),
+          LanguageText(title: 'മലയാളം'),
+          SizedBox(width: 8),
+          LanguageText(title: 'ਪੰਜਾਬੀ'),
+        ],
+      ),
     );
   }
 }
