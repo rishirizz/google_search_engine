14d58345a97b7174fa500ed1482af6ea05ef7f36
arijeet <arijeetc6@gmail.com>
2024-02-28 10:44:14 +0530

    Added image assets.

--- lib/responsive/large_screen.dart
+++ lib/responsive/large_screen.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:google_search_engine/utils/colors.dart';
 
 class LargeScreenWidget extends StatelessWidget {
   const LargeScreenWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: backgroundColor,
+        elevation: 0,
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              'Gmail',
+              style: TextStyle(
+                color: primaryColor,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              'Images',
+              style: TextStyle(
+                color: primaryColor,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          IconButton(onPressed: (){}, icon: Icon(icon))
+        ],
+      ),
+    );
   }
 }
