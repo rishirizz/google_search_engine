919216b6739c0a229ede3726b788a63925804c73
arijeet <arijeetc6@gmail.com>
2024-03-01 20:36:02 +0530

    UI Enhancements

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -24,145 +24,152 @@ class SearchScreen extends StatelessWidget {
       color: Colors.blue,
       title: searchQuery,
       child: Scaffold(
-        body: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SearchHeader(),
-              Padding(
-                padding: EdgeInsets.only(
-                  left: (size.width > 768) ? 160.0 : 20.0,
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SearchHeader(),
+                Padding(
+                  padding: EdgeInsets.only(
+                    left: (size.width > 768) ? 160.0 : 20.0,
+                  ),
+                  child: const SearchTabs(),
                 ),
-                child: const SearchTabs(),
-              ),
-              const Divider(
-                height: 0,
-                thickness: 0.3,
-              ),
-              FutureBuilder(
-                future: apiService.fetchData(
-                  queryTerm: searchQuery,
-                  start: '0',
+                const Divider(
+                  height: 0,
+                  thickness: 0.3,
                 ),
-                builder: (BuildContext context, AsyncSnapshot snapshot) {
-                  if (snapshot.hasData) {
-                    return Column(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(
-                            left: (size.width > 768) ? 160.0 : 20.0,
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Container(
-                                padding: const EdgeInsets.only(
-                                  top: 12,
-                                ),
-                                child: Text(
-                                  'About ${snapshot.data['searchInformation']['formattedTotalResults']} results (${snapshot.data['searchInformation']['formattedSearchTime']} seconds)',
-                                  style: const TextStyle(
-                                    fontSize: 15,
-                                    color: Color(
-                                      0xff70757a,
+                FutureBuilder(
+                  future: apiService.fetchData(
+                    queryTerm: searchQuery,
+                    start: '0',
+                  ),
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    if (snapshot.hasData) {
+                      return Column(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(
+                              left: (size.width > 768) ? 160.0 : 20.0,
+                            ).copyWith(
+                              right: size.width <= 768 ? 20 : 0,
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  padding: const EdgeInsets.only(
+                                    top: 12,
+                                  ),
+                                  child: Text(
+                                    'About ${snapshot.data['searchInformation']['formattedTotalResults']} results (${snapshot.data['searchInformation']['formattedSearchTime']} seconds)',
+                                    style: const TextStyle(
+                                      fontSize: 15,
+                                      color: Color(
+                                        0xff70757a,
+                                      ),
                                     ),
                                   ),
                                 ),
-                              ),
-                              ListView.builder(
-                                shrinkWrap: true,
-                                itemCount: snapshot.data['items'].length,
-                                itemBuilder: (BuildContext context, int index) {
-                                  return Padding(
-                                    padding: const EdgeInsets.only(
-                                      top: 10,
-                                    ),
-                                    child: SearchResultsComponent(
-                                      description: snapshot.data?['items']
-                                          [index]['snippet'],
-                                      link: snapshot.data?['items'][index]
-                                          ['formattedUrl'],
-                                      linkToGo: snapshot.data?['items'][index]
-                                          ['link'],
-                                      data: snapshot.data?['items'][index]
-                                          ['title'],
-                                    ),
-                                  );
-                                },
-                              ),
-                              const SizedBox(
-                                height: 30,
-                              ),
-                              SizedBox(
-                                width: double.infinity,
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    TextButton(
-                                      onPressed: () {
-                                        if (start != '0') {
+                                ListView.builder(
+                                  shrinkWrap: true,
+                                  itemCount: snapshot.data['items'].length,
+                                  itemBuilder:
+                                      (BuildContext context, int index) {
+                                    return Padding(
+                                      padding: const EdgeInsets.only(
+                                        top: 10,
+                                      ),
+                                      child: SearchResultsComponent(
+                                        description: snapshot.data?['items']
+                                            [index]['snippet'],
+                                        link: snapshot.data?['items'][index]
+                                            ['formattedUrl'],
+                                        linkToGo: snapshot.data?['items'][index]
+                                            ['link'],
+                                        data: snapshot.data?['items'][index]
+                                            ['title'],
+                                      ),
+                                    );
+                                  },
+                                ),
+                                const SizedBox(
+                                  height: 30,
+                                ),
+                                SizedBox(
+                                  width: double.infinity,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      TextButton(
+                                        onPressed: () {
+                                          if (start != '0') {
+                                            Navigator.of(context).push(
+                                              MaterialPageRoute(
+                                                builder: (context) =>
+                                                    SearchScreen(
+                                                  searchQuery: searchQuery,
+                                                  start: (int.parse(start) - 10)
+                                                      .toString(), //get prev 10 results
+                                                ),
+                                              ),
+                                            );
+                                          }
+                                        },
+                                        child: const Text(
+                                          '< Prev',
+                                          style: TextStyle(
+                                            fontSize: 15,
+                                            color: blueColor,
+                                          ),
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        width: 20,
+                                      ),
+                                      TextButton(
+                                        onPressed: () {
                                           Navigator.of(context).push(
                                             MaterialPageRoute(
                                               builder: (context) =>
                                                   SearchScreen(
                                                 searchQuery: searchQuery,
-                                                start: (int.parse(start) - 10)
-                                                    .toString(), //get prev 10 results
+                                                start: (int.parse(start) + 10)
+                                                    .toString(), //get next 10 results
                                               ),
                                             ),
                                           );
-                                        }
-                                      },
-                                      child: const Text(
-                                        '< Prev',
-                                        style: TextStyle(
-                                          fontSize: 15,
-                                          color: blueColor,
-                                        ),
-                                      ),
-                                    ),
-                                    const SizedBox(
-                                      width: 20,
-                                    ),
-                                    TextButton(
-                                      onPressed: () {
-                                        Navigator.of(context).push(
-                                          MaterialPageRoute(
-                                            builder: (context) => SearchScreen(
-                                              searchQuery: searchQuery,
-                                              start: (int.parse(start) + 10)
-                                                  .toString(), //get next 10 results
-                                            ),
+                                        },
+                                        child: const Text(
+                                          'Next >',
+                                          style: TextStyle(
+                                            fontSize: 15,
+                                            color: blueColor,
                                           ),
-                                        );
-                                      },
-                                      child: const Text(
-                                        'Next >',
-                                        style: TextStyle(
-                                          fontSize: 15,
-                                          color: blueColor,
                                         ),
                                       ),
-                                    ),
-                                  ],
+                                    ],
+                                  ),
                                 ),
-                              ),
-                              const SizedBox(
-                                height: 30,
-                              ),
-                            ],
+                                const SizedBox(
+                                  height: 30,
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                        const SearchScreenFooter(),
-                      ],
-                    );
-                  }
-                  return const SizedBox();
-                },
-              ),
-            ],
+                          const SearchScreenFooter(),
+                        ],
+                      );
+                    }
+                    return const SizedBox();
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
